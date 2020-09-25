c44f11dcbe3ac9c804b292f0bca3bb911bc8ee99
gramorEticon <56377364+gramorEticon@users.noreply.github.com>
2020-09-26 05:31:40 +0700

    Сделано стартовое активити

--- app/src/main/java/com/eticon/parkoved/MainActivity.kt
+++ app/src/main/java/com/eticon/parkoved/MainActivity.kt
@@ -2,10 +2,26 @@ package com.eticon.parkoved
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var signInUser: ConstraintLayout
+    lateinit var signInPersonal: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signInPersonal = findViewById(R.id.sign_in_personal)
+        signInUser = findViewById(R.id.sign_in_user)
+
+        signInUser.setOnClickListener{
+
+        }
+
+        signInPersonal.setOnClickListener {
+
+        }
     }
 }
