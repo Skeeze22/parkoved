d378d219c0f763b0bd150b8587c40ac41b5588e5
gramorEticon <56377364+gramorEticon@users.noreply.github.com>
2020-09-26 11:15:42 +0700

    Сделано активити выбора парка

--- app/src/main/java/com/eticon/parkoved/CaseParkInCityActivity.kt
+++ app/src/main/java/com/eticon/parkoved/CaseParkInCityActivity.kt
@@ -1,11 +1,31 @@
 package com.eticon.parkoved
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class CaseParkInCityActivity : AppCompatActivity() {
+
+    lateinit var backBtn: ImageView
+    lateinit var park: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_case_park_in_city)
+
+        backBtn = findViewById(R.id.back_btn)
+        park = findViewById(R.id.park)
+
+        backBtn.setOnClickListener {
+            finish()
+        }
+
+        park.setOnClickListener {
+            val intent =  Intent(this, NavigationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
