3ea1d5de8ce0d5a9a4f970ca00b81051656c5898
gramorEticon <56377364+gramorEticon@users.noreply.github.com>
2020-09-26 13:02:07 +0700

    Добавлены фрагменты

--- app/src/main/java/com/eticon/parkoved/NavigationActivity.kt
+++ app/src/main/java/com/eticon/parkoved/NavigationActivity.kt
@@ -1,11 +1,64 @@
 package com.eticon.parkoved
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NavigationActivity : AppCompatActivity() {
+
+    lateinit  var bottomNavigator: BottomNavigationView
+    lateinit var context: Context
+    lateinit var layoutFragment: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        context = this
+
+        initViews()
+        val supportFr = supportFragmentManager
+        val content = supportFr.findFragmentById(R.id.fl_content)
+
+
+
+        if (content == null){
+            val fragment = MapFragment()
+            supportFr.beginTransaction().add(R.id.fl_content , fragment).commit()
+        }
+
+        val mapFragment = MapFragment()
+        val ticketFragmement = TicketFragment()
+
+        bottomNavigator.setOnNavigationItemSelectedListener OnNavigationItemSelectedListener@{  MenuItem ->
+            when(MenuItem.itemId){
+                R.id.map -> {
+                    supportFr.beginTransaction().replace(R.id.fl_content, mapFragment).commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.ticket ->{
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.skaner -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.routes->{
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.profile -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+                else -> return@OnNavigationItemSelectedListener true
+            }
+        }
+
     }
+
+
+    fun initViews(){
+        bottomNavigator = findViewById(R.id.navigation)
+        layoutFragment = findViewById(R.id.fl_content)
+    }
+
 }
