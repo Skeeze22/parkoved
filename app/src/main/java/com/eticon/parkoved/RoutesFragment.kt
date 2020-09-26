cce9ed37f0892af45460e3b3bcfbf49dc25eb0b5
gramorEticon <56377364+gramorEticon@users.noreply.github.com>
2020-09-26 21:00:21 +0700

    Маршруты

--- app/src/main/java/com/eticon/parkoved/RoutesFragment.kt
+++ app/src/main/java/com/eticon/parkoved/RoutesFragment.kt
@@ -5,19 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RoutesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RoutesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+    lateinit var line_1: ConstraintLayout
+    lateinit var line_2: ConstraintLayout
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -33,20 +30,25 @@ class RoutesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_routes, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_routes, container, false)
+
+        line_1 = view.findViewById(R.id.line_1)
+        line_2 = view.findViewById(R.id.line_2)
+
+        line_1.setOnClickListener {
+
+        }
+
+        line_2.setOnClickListener {
+
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RoutesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             RoutesFragment().apply {
